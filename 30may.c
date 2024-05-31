c33db119aecd3eaa37a3c3629cb1f8cd07fcbcf0
Ankit saini <ankitsaini0416@gmail.com>
2024-05-31 11:46:20 +0530

    this is my 30 may project

---
+++ 30may.c
@@ -0,0 +1,221 @@
+// #include <stdio.h>
+// int main(){
+//     int num ;
+//     printf("Enter a number ");
+//     scanf("%d",&num);
+//     if(num >= 50 ){
+//         printf("number is greater than 50");
+//     }else{
+//         printf("Number is not greate than 50");
+//     }
+    
+//     return 0;
+    
+// }
+
+
+
+// #include <stdio.h>
+// int main(){
+//     int number;
+    
+//     printf("Enter a number to ckeck number is greater than 500 or not : ");
+//     scanf("%d",&number);
+    
+//     if(number >= 500){
+//         printf("number is greater than 500 ");
+        
+//     }else{
+//         printf("number is not greater than 500");
+    
+    
+//     }
+//     return 0;
+// }
+
+
+
+
+// #include <stdio.h>
+// int main(){
+//     int num;
+//     printf("Enter a number to check number is even or odd : ");
+//     scanf("%d",&num);
+    
+//     if(num % 2 == 0){
+//         printf("This is even number");
+
+//     }else{
+//         printf("This is odd number");
+//     }
+    
+//     return 0;
+// }
+
+
+
+// #include <stdio.h>
+// int main(){
+//     int num;
+//     printf("Enter a number to check number is div by 7 or not : ");
+//     scanf("%d",&num);
+    
+//     if(num % 7 == 0 ){
+//         printf("This number is div by 7");
+//     }else{
+//      printf("This number is not div by 7");
+//     }
+//     return 0;
+// }
+
+
+// #include <stdio.h>
+// int main(){
+//     int vote;
+//     printf("Enter your age to check you are able to give vote or not : ");
+//     scanf("%d",&vote);
+    
+//     if(vote >= 18){
+//         printf("You can vote");
+//     }
+//     else{
+//         printf("You can't vote");
+        
+//     }
+//     return 0;
+    
+// }
+
+
+
+// #include <stdio.h>
+// int main(){
+//     int num;
+//     printf("Enter a number to check this is one or not : ");
+//     scanf("%d",&num);
+    
+//     if(num == 1 ){
+//         printf("One");
+//     }else{
+//         printf("input is not matched");
+//     }
+// return 0;
+// }
+
+
+
+// #include <stdio.h>
+// int main(){
+//     int num;
+    
+//     printf("Enter a number to check this is one or not : ");
+//     scanf("%d",&num);
+    
+//     if(num == 2){
+//         printf("Two");
+//     }else{
+//         printf("Input is not match");
+//     }
+// return 0;
+    
+// }
+
+
+// #include <stdio.h>
+// int main(){
+//  char word;
+ 
+// printf("Enter a char : ");
+// scanf("%c",&word);
+
+// if(word == 'a'){
+//     printf("Apple");
+    
+// }else{
+//     printf("input is not matched");
+// }
+//     return 0;
+// }
+
+
+// #include <stdio.h>
+// int main(){
+//  char ball;
+ 
+// printf("Enter a char : ");
+// scanf("%c",&ball);
+
+// if(ball == 'b'){
+//     printf("Ball");
+    
+// }else{
+//     printf("input is not matched");
+// }
+//     return 0;
+// }
+
+
+
+
+// #include <stdio.h>
+// int main(){
+//     char vowel;
+//     printf("Enter a char to check is vowel  or consoant ");
+//     scanf("%c",&vowel);
+    
+//     if(vowel == 'a' || vowel == 'e' || vowel == 'i' || vowel == 'o' || vowel == 'u'){
+//         printf("These are vowels");
+//     }else{
+//         printf("These are consoant");
+//     }
+//     return 0;
+// }
+
+
+// #include <stdio.h>
+// int main(){
+//     char word;
+//     printf("Enter a char to check :  ");
+//     scanf("%c",&word);
+    
+//     if(word == 'q' || word == 'w' || word == 'e' || word == 'r' || word == 't' || word == 'y' || word == 'u' || word == 'i' || word == 'o' || word == 'p'){
+//         printf("upper ");
+//     }else{
+//         printf("input is not correct");
+//     }
+//     return 0;
+// }
+
+
+// #include <stdio.h>
+// int main(){
+//     char word;
+//     printf("Enter a char to check :  ");
+//     scanf("%c",&word);
+    
+//     if(word == 'a' || word == 's' || word == 'd' || word == 'f' || word == 'f' || word == 'g' || word == 'h' || word == 'j' || word == 'k' || word == 'l'){
+//         printf("mid line ");
+//     }else{
+//         printf("input is not correct");
+//     }
+//     return 0;
+// }
+
+#include <stdio.h>
+int main(){
+    char word;
+    printf("Enter a char to check :  ");
+    scanf("%c",&word);
+    
+    if(word == 'z' || word == 'x' || word == 'c' || word == 'v' || word == 'b' || word == 'n' || word == 'm'){
+        printf("Lower ");
+    }else{
+        printf("input is not correct");
+    }
+    return 0;
+}
+
+
+
+
+
